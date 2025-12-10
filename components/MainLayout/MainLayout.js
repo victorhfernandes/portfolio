3c017aca3b3bfd104ee389b08766b4a47b4cb076
victorhfernandes <victorh.fernandes.o@gmail.com>
2025-12-10 08:12:45 -0300

    first_commit

---
+++ components/MainLayout/MainLayout.js
@@ -0,0 +1,15 @@
+async function fetchHTML() {
+  const resp = await fetch(new URL("./MainLayout.html", import.meta.url));
+  return await resp.text();
+}
+
+export default class MainLayout extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  async connectedCallback() {
+    this.shadowRoot.innerHTML = await fetchHTML();
+  }
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+import MainLayout from "./components/MainLayout/MainLayout.js";
+window.customElements.define("main-layout", MainLayout)
