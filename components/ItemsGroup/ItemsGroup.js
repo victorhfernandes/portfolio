18e036343b4759ebfefc6f2ae9484414898d5396
victorhfernandes <victorh.fernandes.o@gmail.com>
2025-12-10 22:54:28 -0300

    item_groups_added

---
+++ components/ItemsGroup/ItemsGroup.js
@@ -0,0 +1,45 @@
+async function fetchHTML() {
+  const resp = await fetch(new URL("./ItemsGroup.html", import.meta.url));
+  return await resp.text();
+}
+
+async function fetchData() {
+  const resp = await fetch(
+    "https://script.google.com/macros/s/AKfycby_2CY0Yx6zcLcFjyvldXAAj2JHwS_1Owp0Q7dJWKXurANZaBW3LKReQ-ClUuTa0ubX/exec?path=data&action=read",
+  );
+  return await resp.json();
+}
+
+async function organizeHtmlData() {
+  const data = await fetchData();
+  const htmlString = await fetchHTML();
+
+  const parser = new DOMParser();
+
+  const newHtml = data.data.map((item, index) => {
+    const doc = parser.parseFromString(htmlString, "text/html");
+
+    doc.querySelector(".role").innerHTML = item.role;
+    doc.querySelector(".project").innerHTML = item.project;
+    doc.querySelector("img").src =
+      "https://lh3.googleusercontent.com/d/" + item.cover;
+
+    return doc.body.innerHTML;
+  });
+
+  return newHtml.toString().replaceAll(",", "");
+}
+
+export default class MainLayout extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  async connectedCallback() {
+    this.shadowRoot.innerHTML =
+      (await organizeHtmlData()) +
+      `<link rel="stylesheet" href="/components/ItemsGroup/ItemsGroup.css" />
+`;
+  }
+}
--- index.js
+++ index.js
@@ -3,3 +3,6 @@ window.customElements.define("main-layout", MainLayout);
 
 import NavGroup from "./components/NavGroup/NavGroup.js";
 window.customElements.define("nav-group", NavGroup);
+
+import ItemsGroup from "./components/ItemsGroup/ItemsGroup.js";
+window.customElements.define("items-group", ItemsGroup);
